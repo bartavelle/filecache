d610742548e1133b29496bdbabfd1da5ca8cd763
bartavelle <bartavelle@gmail.com>
2013-06-24 23:17:25 +0200

    Adds the killFileCache function.

--- Data/FileCache.hs
+++ Data/FileCache.hs
@@ -1,4 +1,4 @@
-module Data.FileCache (FileCache, newFileCache, invalidate, query, getCache) where
+module Data.FileCache (FileCache, newFileCache, killFileCache, invalidate, query, getCache) where
 
 import Control.Monad.STM
 import Control.Concurrent.STM
@@ -10,15 +10,19 @@ data Messages a = Invalidate !FilePath
                 | Query !FilePath !(IO a) !(TMVar a)
                 | GetCopy !(TMVar (HM.HashMap FilePath (a, WatchDescriptor)))
 
-data FileCache a = FileCache !(TQueue (Messages a))
+data FileCache a = FileCache !(TQueue (Messages a)) ThreadId
 
 -- | Generates a new file cache. The opaque type is for use with other
 -- functions.
 newFileCache :: IO (FileCache a)
 newFileCache = do
     q <- newTQueueIO
-    forkIO (mapMaster HM.empty q)
-    return (FileCache q)
+    i <- forkIO (mapMaster HM.empty q)
+    return (FileCache q i)
+
+-- | Destroys the thread running the FileCache. Pretty dangerous stuff.
+killFileCache :: FileCache a -> IO ()
+killFileCache (FileCache _ i) = killThread i
 
 mapMaster :: HM.HashMap FilePath (a, WatchDescriptor) -> TQueue (Messages a) -> IO ()
 mapMaster mp q = do
@@ -33,28 +37,28 @@ mapMaster mp q = do
                 Just (x,_) -> atomically (putTMVar respvar x)
                 Nothing -> do
                     val <- action
-                    wm <- withINotify (\ino -> addWatch ino [CloseWrite,Delete,Move] fp (const $ invalidate fp (FileCache q)))
+                    wm <- withINotify (\ino -> addWatch ino [CloseWrite,Delete,Move] fp (const $ invalidate fp (FileCache q undefined)))
                     atomically (putTMVar respvar val)
                     mapMaster (HM.insert fp (val,wm) mp) q
         GetCopy mv -> atomically (putTMVar mv mp) >> mapMaster mp q
 
 -- | Manually invalidates an entry.
 invalidate :: FilePath -> FileCache a -> IO ()
-invalidate fp (FileCache q) = atomically (writeTQueue q (Invalidate fp))
+invalidate fp (FileCache q _) = atomically (writeTQueue q (Invalidate fp))
 
--- | Query the cache, populating it if necessary.
+-- | Queries the cache, populating it if necessary.
 query :: FileCache a
       -> FilePath -- ^ Path of the file entry
       -> IO a -- ^ The computation that will be used to populate the cache
       -> IO a
-query (FileCache q) fp generate = atomically $ do
+query (FileCache q _) fp generate = atomically $ do
     v <- newEmptyTMVar
     writeTQueue q (Query fp generate v)
     readTMVar v
 
--- | Get a copy of the cache.
+-- | Gets a copy of the cache.
 getCache :: FileCache a -> IO (HM.HashMap FilePath (a, WatchDescriptor))
-getCache (FileCache q) = atomically $ do
+getCache (FileCache q _) = atomically $ do
     v <- newEmptyTMVar
     writeTQueue q (GetCopy v)
     readTMVar v
