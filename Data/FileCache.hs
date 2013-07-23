0ca58a68e44e8fd40e9d7347372c9badee1d5f1d
bartavelle <bartavelle@gmail.com>
2013-07-23 14:04:54 +0200

    fix performance problem with multiple inotifies

--- Data/FileCache.hs
+++ Data/FileCache.hs
@@ -5,63 +5,67 @@ import System.INotify
 import Control.Concurrent
 import qualified Data.Either.Strict as S
 import Control.Exception
+import Control.Monad
 
 data Messages a = Invalidate !FilePath
                 | Query !FilePath !(IO (S.Either String a)) !(MVar (S.Either String a))
                 | GetCopy !(MVar (HM.HashMap FilePath (a, WatchDescriptor)))
+                | Stop
 
-data FileCache a = FileCache !(Chan (Messages a)) ThreadId
+data FileCache a = FileCache !(Chan (Messages a))
 
 -- | Generates a new file cache. The opaque type is for use with other
 -- functions.
 newFileCache :: IO (FileCache a)
 newFileCache = do
     q <- newChan
-    i <- forkIO (mapMaster HM.empty q)
-    return (FileCache q i)
+    ino <- initINotify
+    void $ forkIO (mapMaster HM.empty q ino)
+    return (FileCache q)
 
 -- | Destroys the thread running the FileCache. Pretty dangerous stuff.
 killFileCache :: FileCache a -> IO ()
-killFileCache (FileCache _ i) = killThread i
+killFileCache (FileCache q) = writeChan q Stop
 
-mapMaster :: HM.HashMap FilePath (a, WatchDescriptor) -> Chan (Messages a) -> IO ()
-mapMaster mp q = do
+mapMaster :: HM.HashMap FilePath (a, WatchDescriptor) -> Chan (Messages a) -> INotify -> IO ()
+mapMaster mp q ino = do
     msg <- readChan q
     case msg of
+        Stop -> killINotify ino
         Invalidate fp ->
             case HM.lookup fp mp of
-                Nothing -> mapMaster mp q
-                Just (_,desc) -> removeWatch desc >> mapMaster (HM.delete fp mp) q
+                Nothing -> mapMaster mp q ino
+                Just (_,desc) -> removeWatch desc >> mapMaster (HM.delete fp mp) q ino
         Query fp action respvar ->
             case HM.lookup fp mp of
                 Just (x,_) -> putMVar respvar (S.Right x)
                 Nothing -> do
                     valr <- action `catch` (\e -> return (S.Left ("Exception: " ++ show (e :: SomeException))))
                     case valr of
-                        S.Right val -> do
-                            wm <- withINotify (\ino -> addWatch ino [CloseWrite,Delete,Move] fp (const $ invalidate fp (FileCache q undefined)))
+                        S.Right val -> do -- this is blocking for some reason
+                            wm <- addWatch ino [CloseWrite,Delete,Move] fp (const $ invalidate fp (FileCache q))
                             putMVar respvar (S.Right val)
-                            mapMaster (HM.insert fp (val,wm) mp) q
+                            mapMaster (HM.insert fp (val,wm) mp) q ino
                         S.Left rr -> putMVar respvar (S.Left rr)
-        GetCopy mv -> putMVar mv mp >> mapMaster mp q
+        GetCopy mv -> putMVar mv mp >> mapMaster mp q ino
 
 -- | Manually invalidates an entry.
 invalidate :: FilePath -> FileCache a -> IO ()
-invalidate fp (FileCache q _) = writeChan q (Invalidate fp)
+invalidate fp (FileCache q) = writeChan q (Invalidate fp)
 
 -- | Queries the cache, populating it if necessary.
 query :: FileCache a
       -> FilePath -- ^ Path of the file entry
       -> IO (S.Either String a) -- ^ The computation that will be used to populate the cache
       -> IO (S.Either String a)
-query (FileCache q _) fp generate = do
+query (FileCache q) fp generate = do
     v <- newEmptyMVar
     writeChan q (Query fp generate v)
     readMVar v
 
 -- | Gets a copy of the cache.
 getCache :: FileCache a -> IO (HM.HashMap FilePath (a, WatchDescriptor))
-getCache (FileCache q _) = do
+getCache (FileCache q) = do
     v <- newEmptyMVar
     writeChan q (GetCopy v)
     readMVar v
