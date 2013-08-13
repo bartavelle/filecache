511b9377968de6b4b0f4d5345bd49793be2820d8
bartavelle <bartavelle@gmail.com>
2013-08-13 15:12:57 +0200

    Makes the cache more generic and cache errors.

--- Data/FileCache.hs
+++ Data/FileCache.hs
@@ -1,4 +1,4 @@
-module Data.FileCache (FileCache, newFileCache, killFileCache, invalidate, query, getCache) where
+module Data.FileCache (FileCache, FileCacheR, newFileCache, killFileCache, invalidate, query, getCache, lazyQuery) where
 
 import qualified Data.HashMap.Strict as HM
 import System.INotify
@@ -6,17 +6,19 @@ import Control.Concurrent
 import qualified Data.Either.Strict as S
 import Control.Exception
 import Control.Monad
+import Control.Monad.Error.Class
 
-data Messages a = Invalidate !FilePath
-                | Query !FilePath !(IO (S.Either String a)) !(MVar (S.Either String a))
-                | GetCopy !(MVar (HM.HashMap FilePath (a, WatchDescriptor)))
-                | Stop
+data Messages r a = Invalidate !FilePath
+                  | Query !FilePath !(IO (S.Either r a)) !(MVar (S.Either r a))
+                  | GetCopy !(MVar (HM.HashMap FilePath (S.Either r a, WatchDescriptor)))
+                  | Stop
 
-data FileCache a = FileCache !(Chan (Messages a))
+data FileCacheR r a = FileCache !(Chan (Messages r a))
+type FileCache = FileCacheR String
 
 -- | Generates a new file cache. The opaque type is for use with other
 -- functions.
-newFileCache :: IO (FileCache a)
+newFileCache :: Error r => IO (FileCacheR r a)
 newFileCache = do
     q <- newChan
     ino <- initINotify
@@ -24,10 +26,10 @@ newFileCache = do
     return (FileCache q)
 
 -- | Destroys the thread running the FileCache. Pretty dangerous stuff.
-killFileCache :: FileCache a -> IO ()
+killFileCache :: FileCacheR r a -> IO ()
 killFileCache (FileCache q) = writeChan q Stop
 
-mapMaster :: HM.HashMap FilePath (a, WatchDescriptor) -> Chan (Messages a) -> INotify -> IO ()
+mapMaster :: Error r => HM.HashMap FilePath (S.Either r a, WatchDescriptor) -> Chan (Messages r a) -> INotify -> IO ()
 mapMaster mp q ino = do
     let nochange = return (Just mp)
         change x = return (Just (x mp))
@@ -40,36 +42,49 @@ mapMaster mp q ino = do
                     Just (_,desc) -> removeWatch desc >> change (HM.delete fp)
             Query fp action respvar ->
                 case HM.lookup fp mp of
-                    Just (x,_) -> putMVar respvar (S.Right x) >> nochange
+                    Just (x,_) -> putMVar respvar x >> nochange
                     Nothing -> do
-                        valr <- action `catch` (\e -> return (S.Left ("Exception: " ++ show (e :: SomeException))))
-                        case valr of
-                            S.Right val -> do -- this is blocking for some reason
-                                wm <- addWatch ino [CloseWrite,Delete,Move] fp (const $ invalidate fp (FileCache q))
-                                putMVar respvar (S.Right val)
-                                change (HM.insert fp (val,wm))
-                            S.Left rr -> putMVar respvar (S.Left rr) >> nochange
+                        valr <- catch action $ \e -> do
+                            let r = strMsg $ "Exception: " ++ show (e :: SomeException)
+                            return (S.Left r)
+                        wm <- addWatch ino [CloseWrite,Delete,Move,Attrib,Create] fp (const $ invalidate fp (FileCache q))
+                        putMVar respvar valr
+                        change (HM.insert fp (valr,wm))
             GetCopy mv -> putMVar mv mp >> nochange
     case nmp of
         Just x -> mapMaster x q ino
         Nothing -> return ()
 
 -- | Manually invalidates an entry.
-invalidate :: FilePath -> FileCache a -> IO ()
+invalidate :: Error r => FilePath -> FileCacheR r a -> IO ()
 invalidate fp (FileCache q) = writeChan q (Invalidate fp)
 
 -- | Queries the cache, populating it if necessary.
-query :: FileCache a
+query :: Error r
+      => FileCacheR r a
       -> FilePath -- ^ Path of the file entry
-      -> IO (S.Either String a) -- ^ The computation that will be used to populate the cache
-      -> IO (S.Either String a)
+      -> IO (S.Either r a) -- ^ The computation that will be used to populate the cache
+      -> IO (S.Either r a)
 query (FileCache q) fp generate = do
     v <- newEmptyMVar
     writeChan q (Query fp generate v)
     readMVar v
 
+-- | Just like `query`, but with the standard "Either" type.
+lazyQuery :: Error r
+      => FileCacheR r a
+      -> FilePath -- ^ Path of the file entry
+      -> IO (Either r a) -- ^ The computation that will be used to populate the cache
+      -> IO (Either r a)
+lazyQuery q fp generate = fmap unstrict (query q fp (fmap strict generate))
+    where
+        strict (Left x) = S.Left x
+        strict (Right x) = S.Right x
+        unstrict (S.Left x) = Left x
+        unstrict (S.Right x) = Right x
+
 -- | Gets a copy of the cache.
-getCache :: FileCache a -> IO (HM.HashMap FilePath (a, WatchDescriptor))
+getCache :: Error r => FileCacheR r a -> IO (HM.HashMap FilePath (S.Either r a, WatchDescriptor))
 getCache (FileCache q) = do
     v <- newEmptyMVar
     writeChan q (GetCopy v)
