a49b512ff9c6cb5cd9f9560f01fd9385f4618539
bartavelle <bartavelle@gmail.com>
2014-01-31 08:24:49 +0100

    WIP

--- Data/FileCache.hs
+++ Data/FileCache.hs
@@ -13,20 +13,16 @@ module Data.FileCache (FileCache, FileCacheR, newFileCache, killFileCache, inval
 import qualified Data.HashMap.Strict as HM
 import System.INotify
 import Control.Concurrent
+import Control.Concurrent.STM
 import qualified Data.Either.Strict as S
 import Control.Exception
 import Control.Monad
 import Control.Monad.Error.Class
 import Control.Exception.Lens
 
-data Messages r a = Invalidate !FilePath
-                  | Query !FilePath !(IO (S.Either r a)) !(MVar (S.Either r a))
-                  | GetCopy !(MVar (HM.HashMap FilePath (S.Either r a, WatchDescriptor)))
-                  | Stop
-
 -- | The main FileCache type, for queries returning 'Either r a'. The r
 -- type must be an instance of 'Error'.
-data FileCacheR r a = FileCache !(Chan (Messages r a))
+data FileCacheR r a = FileCache !(TVar (HM.HashMap FilePath (S.Either r a, WatchDescriptor))) !INotify
 
 -- | A default type synonym, for String errors.
 type FileCache = FileCacheR String
@@ -34,27 +30,13 @@ type FileCache = FileCacheR String
 -- | Generates a new file cache. The opaque type is for use with other
 -- functions.
 newFileCache :: Error r => IO (FileCacheR r a)
-newFileCache = do
-    q <- newChan
-    ino <- initINotify
-    void $ forkIO (mapMaster HM.empty q ino)
-    return (FileCache q)
+newFileCache = initInotify >>= newTVarIO . FileCache HM.empty
 
 -- | Destroys the thread running the FileCache. Pretty dangerous stuff.
 killFileCache :: FileCacheR r a -> IO ()
-killFileCache (FileCache q) = writeChan q Stop
+killFileCache (FileCache _ ino) = killINotify ino
 
-mapMaster :: Error r => HM.HashMap FilePath (S.Either r a, WatchDescriptor) -> Chan (Messages r a) -> INotify -> IO ()
-mapMaster mp q ino = do
-    let nochange = return (Just mp)
-        change x = return (Just (x mp))
-    msg <- readChan q
-    nmp <- case msg of
-            Stop -> killINotify ino >> return Nothing
-            Invalidate fp ->
-                case HM.lookup fp mp of
-                    Nothing -> nochange
-                    Just (_,desc) -> catching_ id (removeWatch desc) (return ()) >> change (HM.delete fp)
+{-
             Query fp action respvar ->
                 case HM.lookup fp mp of
                     Just (x,_) -> putMVar respvar x >> nochange
@@ -74,10 +56,19 @@ mapMaster mp q ino = do
     case nmp of
         Just x -> mapMaster x q ino
         Nothing -> return ()
+-}
 
 -- | Manually invalidates an entry.
 invalidate :: Error r => FilePath -> FileCacheR r a -> IO ()
-invalidate fp (FileCache q) = writeChan q (Invalidate fp)
+invalidate fp (FileCache q _) = do
+    act <- atomically $ do
+        mp <- readTVar q
+        case HM.lookup fp mp of
+            Nothing -> return (return ())
+            Just (_,desc) -> do
+                writeTVar q (HM.delete fp mp)
+                return (removeWatch desc)
+    act
 
 -- | Queries the cache, populating it if necessary, returning a strict
 -- 'Either' (from "Data.Either.Strict").
@@ -90,10 +81,13 @@ query :: Error r
       -> FilePath -- ^ Path of the file entry
       -> IO (S.Either r a) -- ^ The computation that will be used to populate the cache
       -> IO (S.Either r a)
-query (FileCache q) fp generate = do
-    v <- newEmptyMVar
-    writeChan q (Query fp generate v)
-    readMVar v
+query f@(FileCache q _) fp generate = do
+    act <- atomically $ do
+        mp <- readTVar q
+        case HM.lookup fp mp of
+            Just (x,_) -> return (return x)
+            Nothing -> return $ return ()
+    act
 
 -- | Just like `query`, but with the standard "Either" type.
 lazyQuery :: Error r
@@ -110,8 +104,5 @@ lazyQuery q fp generate = fmap unstrict (query q fp (fmap strict generate))
 
 -- | Gets a copy of the cache.
 getCache :: Error r => FileCacheR r a -> IO (HM.HashMap FilePath (S.Either r a, WatchDescriptor))
-getCache (FileCache q) = do
-    v <- newEmptyMVar
-    writeChan q (GetCopy v)
-    readMVar v
+getCache (FileCache q _) = atomically (readTVar q)
 
