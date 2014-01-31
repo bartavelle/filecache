4f421ca21c36783ae4f0bfbbe223c35ced117905
bartavelle <bartavelle@gmail.com>
2014-01-31 12:55:33 +0100

    Moved from forkIO/Chan to STM

--- Data/FileCache.hs
+++ Data/FileCache.hs
@@ -12,13 +12,13 @@ module Data.FileCache (FileCache, FileCacheR, newFileCache, killFileCache, inval
 
 import qualified Data.HashMap.Strict as HM
 import System.INotify
-import Control.Concurrent
 import Control.Concurrent.STM
 import qualified Data.Either.Strict as S
 import Control.Exception
-import Control.Monad
 import Control.Monad.Error.Class
 import Control.Exception.Lens
+import Control.Applicative
+import Control.Monad (join)
 
 -- | The main FileCache type, for queries returning 'Either r a'. The r
 -- type must be an instance of 'Error'.
@@ -30,45 +30,21 @@ type FileCache = FileCacheR String
 -- | Generates a new file cache. The opaque type is for use with other
 -- functions.
 newFileCache :: Error r => IO (FileCacheR r a)
-newFileCache = initInotify >>= newTVarIO . FileCache HM.empty
+newFileCache = FileCache <$> newTVarIO HM.empty <*> initINotify
 
 -- | Destroys the thread running the FileCache. Pretty dangerous stuff.
 killFileCache :: FileCacheR r a -> IO ()
 killFileCache (FileCache _ ino) = killINotify ino
 
-{-
-            Query fp action respvar ->
-                case HM.lookup fp mp of
-                    Just (x,_) -> putMVar respvar x >> nochange
-                    Nothing -> do
-                        let addw value = do
-                                wm <- addWatch ino [CloseWrite,Delete,Move,Attrib,Create] fp (const $ invalidate fp (FileCache q))
-                                change (HM.insert fp (value,wm))
-                            withWatch value = do
-                                putMVar respvar value
-                                catching_ id (addw value) nochange
-                            noWatch x = putMVar respvar x >> nochange
-                        catches (action >>= withWatch)
-                            [ handler _IOException (\io -> noWatch   (S.Left (strMsg $ show io)))
-                            , handler id           (\e  -> withWatch (S.Left (strMsg $ show e)))
-                            ]
-            GetCopy mv -> putMVar mv mp >> nochange
-    case nmp of
-        Just x -> mapMaster x q ino
-        Nothing -> return ()
--}
-
 -- | Manually invalidates an entry.
 invalidate :: Error r => FilePath -> FileCacheR r a -> IO ()
-invalidate fp (FileCache q _) = do
-    act <- atomically $ do
-        mp <- readTVar q
-        case HM.lookup fp mp of
-            Nothing -> return (return ())
-            Just (_,desc) -> do
-                writeTVar q (HM.delete fp mp)
-                return (removeWatch desc)
-    act
+invalidate fp (FileCache q _) = join $ atomically $ do
+    mp <- readTVar q
+    case HM.lookup fp mp of
+        Nothing -> return (return ())
+        Just (_,desc) -> do
+            writeTVar q (HM.delete fp mp)
+            return (removeWatch desc)
 
 -- | Queries the cache, populating it if necessary, returning a strict
 -- 'Either' (from "Data.Either.Strict").
@@ -81,14 +57,23 @@ query :: Error r
       -> FilePath -- ^ Path of the file entry
       -> IO (S.Either r a) -- ^ The computation that will be used to populate the cache
       -> IO (S.Either r a)
-query f@(FileCache q _) fp generate = do
-    act <- atomically $ do
-        mp <- readTVar q
-        case HM.lookup fp mp of
-            Just (x,_) -> return (return x)
-            Nothing -> return $ return ()
-    act
-
+query f@(FileCache q ino) fp action = do
+    mp <- getCache f
+    case HM.lookup fp mp of
+        Just (x,_) -> return x
+        Nothing -> do
+            let addw value = do
+                    wm <- addWatch ino [CloseWrite,Delete,Move,Attrib,Create] fp (const $ invalidate fp f)
+                    change (HM.insert fp (value,wm))
+                withWatch value = do
+                    catching_ id (addw value) nochange
+                    return value
+                change = atomically . modifyTVar q
+                nochange = return ()
+            catches (action >>= withWatch)
+                [ handler _IOException (\io -> return    (S.Left (strMsg $ show io)))
+                , handler id           (\e  -> withWatch (S.Left (strMsg $ show e)))
+                ]
 -- | Just like `query`, but with the standard "Either" type.
 lazyQuery :: Error r
       => FileCacheR r a
